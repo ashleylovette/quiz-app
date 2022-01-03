40316431d64e6c75ab42d698991be85a2bb3b6f0
ashleylovette <ashleylovette15@gmail.com>
2022-01-03 15:35:48 -0600

    saving high scores

--- end.js
+++ end.js
@@ -4,12 +4,27 @@ const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
     console.log("saved!");
-    // console.log(username.value);
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+    highScores.push(score);
+    highScores.sort((a,b) =>b.score - a.score)
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
+
+    console.log(highScores);
 } 
